eb243a724c84b5963dfc10ab97243f19f7ed3402
Koussai21 <koussailaribi21@gmail.com>
2024-03-29 13:59:48 +0100

    new file:   backend/.gitignore 	new file:   backend/package-lock.json 	new file:   backend/package.json 	new file:   backend/server.js 	new file:   frontend/.gitignore 	new file:   frontend/README.md 	new file:   frontend/package-lock.json 	new file:   frontend/package.json 	new file:   frontend/public/favicon.ico 	new file:   frontend/public/index.html 	new file:   frontend/public/logo192.png 	new file:   frontend/public/logo512.png 	new file:   frontend/public/manifest.json 	new file:   frontend/public/robots.txt 	new file:   frontend/src/App.css 	new file:   frontend/src/App.js 	new file:   frontend/src/App.test.js 	new file:   frontend/src/index.css 	new file:   frontend/src/index.js 	new file:   frontend/src/logo.svg 	new file:   frontend/src/reportWebVitals.js 	new file:   frontend/src/setupTests.js

---
+++ backend/server.js
@@ -0,0 +1,51 @@
+// Import des modules nécessaires
+const express = require('express');
+const mongoose = require('mongoose');
+
+// Création de l'application Express
+const app = express();
+
+// Connexion à la base de données MongoDB
+mongoose.connect('mongodb://localhost:27017/Ikeo', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(() => {
+    console.log('Connexion à la base de données réussie');
+})
+.catch((err) => {
+    console.error('Erreur de connexion à la base de données :', err);
+    process.exit(1); // Arrêter le processus en cas d'erreur de connexion
+});
+
+// Schéma de données MongoDB
+const tableSchema = new mongoose.Schema({
+    Nom: String,
+    Image: String,
+    Prix: Number,
+    Matériau: String,
+    Référence: String
+}, { collection: 'Tables' }); // Spécification de la collection
+
+// Modèle MongoDB basé sur le schéma
+const Table = mongoose.model('Table', tableSchema);
+
+// Route pour afficher les données depuis la collection "tables" de la base de données
+app.get('/tables', async (req, res) => {
+    try {
+        // Récupérer toutes les tables depuis la collection "tables" de la base de données
+        const tables = await Table.find();
+        res.json(tables); // Renvoyer les données au format JSON
+    } catch (err) {
+        console.error('Erreur lors de la récupération des tables :', err);
+        res.status(500).json({ message: 'Erreur serveur' });
+    }
+});
+
+// Port d'écoute du serveur
+const port = 3000;
+
+// Démarrer le serveur
+app.listen(port, () => {
+    console.log(`Serveur démarré sur le port ${port}`);
+});

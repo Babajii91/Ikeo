7559af76944e7b37b5a9122fac35a58c688f5ffd
Koussai21 <koussailaribi21@gmail.com>
2024-03-29 14:05:56 +0100

    modified:   frontend/src/App.js 	new file:   frontend/src/components/Layout/Footer.js 	new file:   frontend/src/components/Layout/Header.js 	new file:   frontend/src/components/Layout/Layout.js 	new file:   frontend/src/pages/Authentification.js 	new file:   frontend/src/pages/Connexion.js 	new file:   frontend/src/pages/Hompage.js 	new file:   frontend/src/pages/Inscription.js 	new file:   frontend/src/pages/PanierAchat.js 	new file:   frontend/src/pages/ProfilUtilisateur.js

---
+++ frontend/src/pages/Connexion.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Layout from "../components/Layout/Layout";
+
+const Connexion = () => {
+    return(
+        <Layout>
+            <h1>Connexion</h1>
+        </Layout>
+    );
+};
+
+export default Connexion;

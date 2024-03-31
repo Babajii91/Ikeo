8218bdc7cd2bfca8eee588b8965f9866148fd7a9
Babajii91 <jrakotonirina91@gmail.com>
2024-03-31 20:09:00 +0200

    new file:   frontend/Images/facebook.png 	new file:   frontend/Images/instagram.png 	new file:   frontend/Images/twitter.png 	modified:   frontend/src/components/Layout/Footer.css 	modified:   frontend/src/components/Layout/Footer.js 	modified:   frontend/src/pages/Homepage.css 	modified:   frontend/src/pages/Homepage.js 	new file:   frontend/src/pages/Inscription.css 	modified:   frontend/src/pages/Inscription.js

--- frontend/src/components/Layout/Footer.js
+++ frontend/src/components/Layout/Footer.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Footer.css";
 
+
 const Footer = () => {
     return (
         <div className="bg-dark text-light p-5">
@@ -9,9 +10,10 @@ const Footer = () => {
                     <div className="col-md-6">
                         <h4>Restez Connecté</h4>
                         <ul className="list-unstyled footer-links">
-                            <li><a href="#"><i className="fab fa-facebook-f"></i> Facebook</a></li>
-                            <li><a href="#"><i className="fab fa-twitter"></i> Twitter</a></li>
-                            <li><a href="#"><i className="fab fa-instagram"></i> Instagram</a></li>
+                        <li><a href="#"><img src="/images/facebook.png" alt="Facebook" /></a></li>
+                        <li><a href="#"><img src="/images/twitter.png" alt="Twitter" /></a></li>
+                        <li><a href="#"><img src="/images/instagram.png" alt="Instagram" /></a></li>
+
                             {/* Ajoutez d'autres icônes de médias sociaux et leurs liens */}
                         </ul>
                     </div>

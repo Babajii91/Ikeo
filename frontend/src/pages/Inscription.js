71a41de0cac1c44a6c8de98886fa399fef7ac204
Koussai21 <koussailaribi21@gmail.com>
2024-03-31 15:53:03 +0200

    modified:   frontend/src/pages/Inscription.js

--- frontend/src/pages/Inscription.js
+++ frontend/src/pages/Inscription.js
@@ -1,63 +1,71 @@
-// Inscription.js
+    // Inscription.js
+    import React, { useState } from 'react';
+    import axios from 'axios';
+    import Layout from "../components/Layout/Layout";
 
-import React, { useState } from 'react';
-import axios from 'axios';
+    function Inscription() {
+        const [formData, setFormData] = useState({
+            nom: '',
+            prenom: '',
+            date_naissance: '',
+            email: '',
+            password: '' // Nouveau champ pour le mot de passe
+        });
 
-function Inscription() {
-    const [formData, setFormData] = useState({
-        nom: '',
-        prenom: '',
-        date_naissance: '',
-        email: ''
-    });
+        const handleChange = (e) => {
+            const { name, value } = e.target;
+            setFormData(prevState => ({
+                ...prevState,
+                [name]: value
+            }));
+        };
 
-    const handleChange = (e) => {
-        const { name, value } = e.target;
-        setFormData(prevState => ({
-            ...prevState,
-            [name]: value
-        }));
-    };
+        const handleSubmit = async (e) => {
+            e.preventDefault();
+            try {
+                await axios.post('http://localhost:3000/users', formData); // Modifier l'URL pour correspondre à la route appropriée
+                alert('Inscription réussie !');
+            } catch (error) {
+                console.error('Erreur lors de l\'inscription:', error);
+                alert('Une erreur est survenue lors de l\'inscription.');
+            }
+        };
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        try {
-            await axios.post('http://localhost:3000/users', formData); // Modifier l'URL pour correspondre à la route appropriée
-            alert('Inscription réussie !');
-        } catch (error) {
-            console.error('Erreur lors de l\'inscription:', error);
-            alert('Une erreur est survenue lors de l\'inscription.');
-        }
-    };
+        return (
+            <Layout>
+                <div>
+                <h1>Inscription</h1>
+                <form onSubmit={handleSubmit}>
+                    <label>
+                        Nom:
+                        <input type="text" name="nom" value={formData.nom} onChange={handleChange} />
+                    </label>
+                    <br />
+                    <label>
+                        Prénom:
+                        <input type="text" name="prenom" value={formData.prenom} onChange={handleChange} />
+                    </label>
+                    <br />
+                    <label>
+                        Date de naissance:
+                        <input type="date" name="date_naissance" value={formData.date_naissance} onChange={handleChange} />
+                    </label>
+                    <br />
+                    <label>
+                        Email:
+                        <input type="email" name="email" value={formData.email} onChange={handleChange} />
+                    </label>
+                    <br />
+                    <label>
+                        Mot de passe:
+                        <input type="password" name="password" value={formData.password} onChange={handleChange} />
+                    </label>
+                    <br />
+                    <button type="submit">S'inscrire</button>
+                </form>
+            </div>
+            </Layout>
+        );
+    }
 
-    return (
-        <div>
-            <h1>Inscription</h1>
-            <form onSubmit={handleSubmit}>
-                <label>
-                    Nom:
-                    <input type="text" name="nom" value={formData.nom} onChange={handleChange} />
-                </label>
-                <br />
-                <label>
-                    Prénom:
-                    <input type="text" name="prenom" value={formData.prenom} onChange={handleChange} />
-                </label>
-                <br />
-                <label>
-                    Date de naissance:
-                    <input type="date" name="date_naissance" value={formData.date_naissance} onChange={handleChange} />
-                </label>
-                <br />
-                <label>
-                    Email:
-                    <input type="email" name="email" value={formData.email} onChange={handleChange} />
-                </label>
-                <br />
-                <button type="submit">S'inscrire</button>
-            </form>
-        </div>
-    );
-}
-
-export default Inscription;
+    export default Inscription;

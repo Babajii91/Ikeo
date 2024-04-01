a4e114d73dc554f04c5a46fd2c79f5363d0392ad
Koussai21 <koussailaribi21@gmail.com>
2024-04-01 10:25:16 +0200

    modified:   backend/package-lock.json 	modified:   backend/package.json 	modified:   backend/server.js

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,15 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const bcrypt = require('bcrypt');
+const bodyParser = require('body-parser');
 
 // Création de l'application Express
 const app = express();
 
 app.use(cors());
+app.use(express.json());
+app.use(bodyParser.json());
 
 // Connexion à la base de données MongoDB
 mongoose.connect('mongodb://localhost:27017/Ikeo', {
@@ -33,6 +37,19 @@ const tableSchema = new mongoose.Schema({
 // Modèle MongoDB basé sur le schéma
 const Table = mongoose.model('Table', tableSchema);
 
+// Schéma de données MongoDB pour la collection "User"
+const userSchema = new mongoose.Schema({
+    nom: String,
+    prenom: String,
+    date_naissance: Date,
+    email: String,
+    password: String
+  }, { collection: 'User' });
+
+
+// Modèle MongoDB basé sur le schéma
+const User = mongoose.model('User', userSchema);
+
 // Route pour afficher les données depuis la collection "Tables" de la base de données
 app.get('/tables', async (req, res) => {
     try {
@@ -45,18 +62,6 @@ app.get('/tables', async (req, res) => {
     }
 });
 
-// Schéma de données MongoDB pour la collection "User"
-const userSchema = new mongoose.Schema({
-    nom: String,
-    prenom: String,
-    date_naissance: Date,
-    email: String
-}, { collection: 'User' });
-
-// Modèle MongoDB basé sur le schéma
-const User = mongoose.model('User', userSchema);
-
-// Route pour afficher les données depuis la collection "User" de la base de données
 app.get('/users', async (req, res) => {
     try {
         // Récupérer tous les utilisateurs depuis la collection "User" de la base de données
@@ -65,8 +70,18 @@ app.get('/users', async (req, res) => {
     } catch (err) {
         console.error('Erreur lors de la récupération des utilisateurs :', err);
         res.status(500).json({ error: err.message });
+    }});
+
+// Route pour créer un nouvel utilisateur
+app.post('/users', async (req, res) => {
+    try {
+      const user = new User(req.body);
+      await user.save();
+      res.status(201).json({ message: 'Inscription réussie !' });
+    } catch (error) {
+      res.status(400).json({ message: 'Une erreur est survenue lors de l\'inscription.' });
     }
-});
+  });
 
 // Port d'écoute du serveur
 const port = 3000;
